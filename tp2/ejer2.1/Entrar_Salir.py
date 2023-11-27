41246e7431c85499b52394be9aec753ed2b37652
Carlos Castillo <carlos.castisho@gmail.com>
2023-11-27 19:36:55 -0300

    Ejer2.1

---
+++ tp2/ejer2.1/Entrar_Salir.py
@@ -0,0 +1,14 @@
+class Entrar_Salir:
+    def __init__(self):
+        pass
+    
+    def __enter__(self):
+        print("Cambiando el directorio")
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print("Saliendo del directorio")
+
+with Entrar_Salir():
+    res = 10-4
+    print(res)
